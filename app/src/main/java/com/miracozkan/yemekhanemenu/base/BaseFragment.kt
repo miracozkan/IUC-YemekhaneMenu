0af020f120a4188af6cd9ca9e2a2c04d0e699e27
Miraç Özkan <mirac.ozkan123@gmail.com>
2019-11-22 19:51:24 +0300

    added dimens xml

---
+++ app/src/main/java/com/miracozkan/yemekhanemenu/base/BaseFragment.kt
@@ -0,0 +1,15 @@
+package com.miracozkan.yemekhanemenu.base
+
+import androidx.fragment.app.Fragment
+
+
+// Code with ❤
+//┌─────────────────────────────┐
+//│ Created by Mirac OZKAN      │
+//│ ─────────────────────────── │
+//│ mirac.ozkan123@gmail.com    │
+//│ ─────────────────────────── │
+//│ 22.11.2019 - 19:34          │
+//└─────────────────────────────┘
+
+open class BaseFragment : Fragment()
